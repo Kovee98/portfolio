01ddd435c6e79af66b7f0fb0bcafb67e7dbd1bf5
kovee98 <kovee98@gmail.com>
2021-03-28 15:33:37 -0500

    refactor: retreive items from couchdb

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
     <div id="app" class="has-background-light">
         <Header/>
-        <Projects/>
-        <Jobs/>
-        <Education/>
+        <Projects :projects="projects"/>
+        <Jobs :jobs="jobs"/>
+        <Education :education="education"/>
         <Footer/>
     </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+    import { defineComponent, onBeforeMount, reactive } from 'vue';
     import Header from './components/Header.vue';
     import Jobs from './components/Jobs.vue';
     import Projects from './components/Projects.vue';
@@ -24,6 +24,41 @@
             Projects,
             Education,
             Footer
+        },
+        setup () {
+            let projects = reactive([]);
+            let jobs = reactive([]);
+            let education = reactive([]);
+
+            onBeforeMount(() => {
+                const auth = btoa('portfolio:QbS7t3xFaz4Xi3');
+
+                fetch('http://couchdb.kovalchik.cloud/portfolio/_all_docs?include_docs=true', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Basic ${auth}`
+                    }
+                })
+                    .then((res) => res.json())
+                    .then((res) => res.rows)
+                    .then((docs) => {
+                        if (docs) {
+                            projects = docs.find((doc: any) => doc.id === 'projects').doc.items;
+                            jobs = docs.find((doc: any) => doc.id === 'experience').doc.items;
+                            education = docs.find((doc: any) => doc.id === 'education').doc.items;
+
+                            console.log('projects:', projects);
+                            console.log('jobs:', jobs);
+                            console.log('education:', education);
+                        }
+                    });
+            });
+
+            return {
+                projects,
+                jobs,
+                education
+            };
         }
     });
 </script>
